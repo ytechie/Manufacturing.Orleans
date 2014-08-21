35d5ad1889758b34f7856f372a8b9659fb549036
Jason Young <jason@ytechie.com>
2014-08-21 14:28:32 -0700

    Stubbed in an orleans project for the mfx

---
+++ GrainCollection/Grain1.cs
@@ -0,0 +1,13 @@
+﻿using GrainInterfaces;
+
+namespace GrainCollection
+{
+    /// <summary>
+    /// Orleans grain implementation class Grain1.
+    /// </summary>
+    public class Grain1 : Orleans.GrainBase, IGrain1
+    {
+        // TODO: replace placeholder grain interface with actual grain
+        // communication interface(s).
+    }
+}
---
+++ GrainInterfaces/IGrain1.cs
@@ -0,0 +1,15 @@
+﻿namespace GrainInterfaces
+{
+    /// <summary>
+    /// Orleans grain communication interface IGrain1
+    /// </summary>
+    public interface IGrain1 : Orleans.IGrain
+    {
+        // TODO: add your interface methods.
+        //
+        // All methods and property getters must return a Task or Task<T>.
+        //
+        // Use explicit setter methods returning Task instead of property setters,
+        // since regular property setters cannot return Task.
+    }
+}
